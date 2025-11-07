29ce5b4f9e638a04b7b4a0677a9e5e34dba79c39
Isaac Garcia <isaacgama120392@outlook.com>
2025-11-07 00:37:08 -0600

    Se agregan variables

--- main.tf
+++ main.tf
@@ -1,14 +1,39 @@
 #### Para esta parte, se estara creando una instancia EC2, con gninx, security groups
 #### y tags
 
+# Variables
+variable "ami_id"{
+    description = "ID de la AMI para la instania EC2"
+    default = "ami-0440d3b780d96b29d"
+}
+
+variable "instance_type"{
+    description = "Tipo de instancia EC2"
+    default = "t3.micro"
+}
+
+variable "server_name" {
+    description = "Nombre del servidor web"
+    default = "nginx-server"
+}
+
+variable "environment"{
+    description = "Ambiente de la Aplicacion"
+    default = "test"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "name" {
+# Instancia EC2
+resource "aws_instance" "nginx-server" {
     # Amazon image
-    ami = "ami-0440d3b780d96b29d"
-    instance_type = "t3.micro"
+    # ami = "ami-0440d3b780d96b29d"
+    # instance_type = "t3.micro"
+
+    ami = var.ami_id
+    instance_type = var.instance_type
 
     # Script para instalar nginx
     user_data = <<-EOF
@@ -26,8 +51,10 @@ resource "aws_instance" "name" {
     ]
 
     tags = {
-        Name = "nginx-server"
-        Environment = "test"
+        # Name = "nginx-server"
+        Name = var.server_name
+        # Environment = "test"
+        Environment = var.environment
         Owner = "cosmic geralt"
         Team = "Devops"
         Project = "mine"
@@ -36,14 +63,17 @@ resource "aws_instance" "name" {
 
 # Creamos un  ssh key
 # ssh-keygen -t rsa -b 2048 -f "nginx-server.key"
+# Si se requiere crear mas de 2 servers, se tienen que crear sus llaves
+# correspondientes
 # ssh user@ip -i priv_key
+# concatenamos valor de vars: ${var.server_name} entre comillas
 resource "aws_key_pair" "nginx-server-ssh"{
-    key_name = "nginx-server-ssh"
+    key_name = "${var.server_name}-ssh"
     # Donde esta el archivo 
-    public_key = file("nginx-server.key.pub")
+    public_key = file("${var.server_name}.key.pub")
 
     tags = {
-        Name = "nginx-server-ssh"
+        Name = "${var.server_name}-ssh"
         Environment = "test"
         Owner = "cosmic geralt"
         Team = "Devops"
@@ -99,5 +129,5 @@ output "server_public_ip" {
 
 output "server_public_dns" {
     description = "DNS publico de la instancia EC2"
-    value = aws_instance.nginx-server.ublic_dns
+    value = aws_instance.nginx-server.public_dns
 }
